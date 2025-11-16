8aaffe4bc1863422adc78c1bb5a83e313befad48
jahidbappi <jahidcric2000@gamil.com>
2025-11-16 15:07:09 +0600

    Initial Commit

---
+++ Compiler Lab/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Compiler Lab
+//
+//  Created by Jahid Bappi on 16/11/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
